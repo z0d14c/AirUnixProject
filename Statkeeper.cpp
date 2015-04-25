58c0cfd41283a316f3a57198b376d4cee9c88f31
beeclo9 <beeclo9@gmail.com>
2015-04-25 18:44:48 -0500

    Update Statkeeper.cpp

--- Statkeeper.cpp
+++ Statkeeper.cpp
@@ -1,22 +1,54 @@
+
 #include "Statkeeper.h"
 
 Statkeeper::Statkeeper(int simTime){
 }
-void Statkeeper::plusTakeoffPlanes(){
+	void Statkeeper::plusTakeoffPlanes()
+	{
+		this.takeoffPlanes++;
+	}
+	
+	void Statkeeper::plusLandedPlanes()
+	{
+		this.landedPlanes++;
+	}
+	
+	void Statkeeper::plusCrashPlanes()
+	{
+		this.crashedPlanes++;
+	}	
+	
+	void Statkeeper::plusTakeoffTime(int time)
+	{
+		this.takeoffTime++;
+		
+	}	
+	void Statkeeper::plusLandingTime(int time)
+	{
+		this.landingTime++;
+	}
+	
+	int Statkeeper::avgTakeoffTime()
+	{
+		double avg1;
+		avg1 = takeoffTime / takeoffPlanes;		
+		return 0;
 }
-void Statkeeper::plusLandedPlanes(){
+	int Statkeeper::avgLandingTime()
+	{		
+		double avg2;
+		avg2 = landedPlanes / landedPlanes;		
+		return 0;
 }
-void Statkeeper::plusCrashPlanes(){
-}
-void Statkeeper::plusTakeoffTime(int time){
-}
-void Statkeeper::plusLandingTime(int time){
-}
-int Statkeeper::avgTakeoffTime(){
-return 0;
-}
-int Statkeeper::avgLandingTime(){
-return 0;
-}
-void Statkeeper::printStats(){
+	void Statkeeper::printStats()
+	{
+		
+		
+		cout << "Total simulation time: " << simTime << endl;
+		cout << "The number of planes that took off: " << takeoffPlanes << endl;
+		cout << "The number of planes that landed: " << landedPlanes << endl;
+		cout << "The number of planes crashed: " << crashedPlanes << endl;
+		cout << "The average time a plane in the takeoff queue: " << avg1 << endl;
+		cout << "The average time a plane in the landing queue: " << avf2 << endl;
+		
 }
